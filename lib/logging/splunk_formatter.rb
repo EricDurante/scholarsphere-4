25b2a4aebb62ea157ec298c33af1be63b6e00015
Dann Bohn <djb44@psu.edu>
2020-03-16 16:19:39 -0400

    adds newline

--- lib/logging/splunk_formatter.rb
+++ lib/logging/splunk_formatter.rb
@@ -1,9 +1,12 @@
-class SplunkFormatter < ::Logger::Formatter
+class SplunkFormatter
+
     def call(severity, timestamp, _progname, message)
-        { 
+        msg = { 
             type: severity,
             time: timestamp,
             message: message
           }.to_json
+        "#{msg} \n"
     end
+
 end
