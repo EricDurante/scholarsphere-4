2095bbfa007d0111de2ade262ec75ea07ee6204c
Dann Bohn <djb44@psu.edu>
2020-04-15 13:53:14 -0400

    name error

--- app/jobs/clam/clam_job.rb
+++ app/jobs/clam/clam_job.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class ClamJob < ApplicationJob
+class Clam::ClamJob < ApplicationJob
     queue_as :clam
    
     ## Job is performed by clam-listener
--- app/jobs/shrine/promotion_job.rb
+++ app/jobs/shrine/promotion_job.rb
@@ -9,7 +9,7 @@ class Shrine::PromotionJob < ApplicationJob
     attacher = Shrine::Attacher.retrieve(model: record, name: name.to_sym, file: file_data)
     attacher.atomic_promote
     # TODO put this somewhere else
-    ClamJob.perform_later(file_data)
+    Clam::ClamJob.perform_later(file_data)
   rescue Shrine::AttachmentChanged, ActiveRecord::RecordNotFound
     # attachment has changed or record has been deleted, nothing to do
   end
